1fda30844c985a91bb0da7351bbf29251a59f2cf
jonasbdc1998 <jonasbdc1998@gmail.com>
2020-11-15 15:36:52 -0300

    db connection

---
+++ src/database/conection.js
@@ -0,0 +1,33 @@
+const sqlite3 = require('sqlite3').verbose();
+
+const DBSOURCE = "dellen.db"
+
+let db = new sqlite3.Database(DBSOURCE,(err) => {
+    if (err) {
+      return console.error(err.message);
+    }
+    console.log('Connected to the SQlite database.');
+  });
+
+  db.run(`CREATE TABLE user (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name text, 
+    email text UNIQUE, 
+    password text, 
+    CONSTRAINT email_unique UNIQUE (email)
+    )`,(err) => {
+    if(err){
+        //Table already created
+        console.log('table already created.')
+    }else{
+        //seeds 
+        console.log('create seeds...')
+        var insert = 'INSERT INTO user (name, email, password) VALUES (?,?,?)'
+        db.run(insert,['user_test','test@test.com','test321'])
+    }
+    }
+);
+
+db.close()
+
+module.exports = db
--- src/server.js
+++ src/server.js
@@ -3,11 +3,12 @@ const cors = require('cors');
 const routes = require('./routes');
 
 const app = express();
+const port = 3333
 
 app.use(express.json());
 app.use(cors())
 app.use(routes)
 
-app.listen(3333, () => {
-    console.log(`Example app listening at http://localhost:${3333}`)
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
   })
